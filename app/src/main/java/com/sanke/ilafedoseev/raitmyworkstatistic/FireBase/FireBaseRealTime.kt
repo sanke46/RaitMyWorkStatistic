805313b2a97146ad6f45a574f1fbb6a37525c9b4
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-12-12 15:11:48 +0300

    some

--- app/src/main/java/com/sanke/ilafedoseev/raitmyworkstatistic/FireBase/FireBaseRealTime.kt
+++ app/src/main/java/com/sanke/ilafedoseev/raitmyworkstatistic/FireBase/FireBaseRealTime.kt
@@ -3,17 +3,50 @@ package com.sanke.ilafedoseev.raitmyworkstatistic.FireBase
 import android.widget.TextView
 import com.google.firebase.database.*
 
-class FireBaseRealTime {
+class FireBaseRealTime(val nameLocation: String ) {
+    lateinit var listofStarsCounter : ArrayList<Int>
+    lateinit var listOfLikesInTab : ArrayList<Int>
 
     var mDataBase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
     var mDeviceRef = mDataBase.child("devices")
     var mTabloRef = mDataBase.child("tablo")
-    var mDeviceLikesRef = mDataBase.child("service").child("devices").child("sam").child("likes")
-    var mTabloLikesRef = mDataBase.child("tablo").child("sam").child("likes")
+    var mDeviceLikesRef = mDataBase.child(nameLocation).child("devices").child("sam").child("likes")
+    var mTabloLikesRef = mDataBase.child(nameLocation).child("tablo").child("tabloOne").child("likes")
 
-    fun allAverageLike(text : TextView) {
-        var count : Int = 0
-        var sumAllvalue : Int = 0
+
+    fun allTabLikesAndDislike(tabLikeNumber: TextView, tabDislikeNumber: TextView) {
+        mTabloLikesRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot : DataSnapshot?) {
+                listOfLikesInTab = ArrayList<Int>(listOf(0,0))
+                dataSnapshot?.children?.forEach { child : DataSnapshot ->
+                    val objetTabMap : MutableMap<String, Any>
+                    objetTabMap = child.value as MutableMap<String, Any>
+
+                    for (entry in objetTabMap) {
+                        if(entry.key.equals("value")) {
+                            when(entry.value.toString()) {
+                                "true" -> listOfLikesInTab.set(0, listOfLikesInTab.get(0) + 1)
+                                "false" -> listOfLikesInTab.set(1, listOfLikesInTab.get(1) + 1)
+                            }
+                        }
+                    }
+
+                    tabLikeNumber.text = listOfLikesInTab.get(0).toString()
+                    tabDislikeNumber.text = listOfLikesInTab.get(1).toString()
+
+                }
+            }
+
+        })
+    }
+
+    fun allAverageLike(text: TextView, star1: TextView, star2: TextView, star3: TextView, star4: TextView, star5: TextView, allReview : TextView) {
+        var count : Int
+        var sumAllvalue : Int
 
         mDeviceLikesRef.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError?) {
@@ -21,23 +54,43 @@ class FireBaseRealTime {
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot?) {
-                for (child : DataSnapshot in dataSnapshot?.children!!) {
-                    var objectMap : Map<String, Any>
-                    objectMap = child.value as Map<String, Any>;
+                listofStarsCounter = ArrayList<Int>(listOf(0,0,0,0,0))
+                count = 0
+                sumAllvalue = 0
+
+                dataSnapshot?.children?.forEach { child : DataSnapshot ->
+
+                    val objectMap : MutableMap<String, Any>
+                    objectMap = child.value as MutableMap<String, Any>
+
 
                     for(entary in objectMap) {
                         if(entary.key.equals("value")) {
+                            when(entary.value.toString()){
+                                "1" -> listofStarsCounter.set(0, listofStarsCounter.get(0) + 1)
+                                "2" -> listofStarsCounter.set(1, listofStarsCounter.get(1) + 1)
+                                "3" -> listofStarsCounter.set(2, listofStarsCounter.get(2) + 1)
+                                "4" -> listofStarsCounter.set(3, listofStarsCounter.get(3) + 1)
+                                "5" -> listofStarsCounter.set(4, listofStarsCounter.get(4) + 1)
+                            }
                             count++
                             sumAllvalue += entary.value.toString().toInt()
                         }
                     }
                 }
+                try {
+                    text.text = "%.1f".format(sumAllvalue.toDouble() / count.toDouble())
+                    allReview.text = "За все время : " + count.toString()
+                    star1.text = "%.1f".format((100.0/count.toDouble()) * listofStarsCounter.get(0).toDouble()) + "%"
+                    star2.text = "%.1f".format((100.0 / count.toDouble()) * listofStarsCounter.get(1).toDouble()) + "%"
+                    star3.text = "%.1f".format((100.0 / count.toDouble()) * listofStarsCounter.get(2).toDouble()) + "%"
+                    star4.text = "%.1f".format((100.0 / count.toDouble()) * listofStarsCounter.get(3).toDouble()) + "%"
+                    star5.text = "%.1f".format((100.0 / count.toDouble()) * listofStarsCounter.get(4).toDouble()) + "%"
+                } catch (e : ArithmeticException) {
 
-                println(sumAllvalue) // 9
-                println(count) // 3
-                println("!!!!!!!!!!!!")
+                }
 
-                text.text = "%.1f".format(sumAllvalue.toDouble()/count.toDouble())
+
             }
         })
     }
--- app/src/main/java/com/sanke/ilafedoseev/raitmyworkstatistic/MainActivity.kt
+++ app/src/main/java/com/sanke/ilafedoseev/raitmyworkstatistic/MainActivity.kt
@@ -1,18 +1,61 @@
 package com.sanke.ilafedoseev.raitmyworkstatistic
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.sanke.ilafedoseev.raitmyworkstatistic.FireBase.FireBaseRealTime
 
+
+
 class MainActivity : AppCompatActivity() {
+
+//    var mDemoCollectionPagerAdapter: DemoCollectionPagerAdapter? = null
+    var mViewPager: ViewPager? = null
+
+    lateinit var textPercent : TextView
+    lateinit var starPercent5 : TextView
+    lateinit var starPercent4 : TextView
+    lateinit var starPercent3 : TextView
+    lateinit var starPercent2 : TextView
+    lateinit var starPercent1 : TextView
+    lateinit var allReview : TextView
+
+    lateinit var tabLikeNumber : TextView
+    lateinit var tabDislikeNumber : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var fireBase = FireBaseRealTime()
-        var text : TextView = findViewById(R.id.averageInterest) as TextView
-        fireBase.allAverageLike(text)
-    }
 
+//        mDemoCollectionPagerAdapter =
+//                 DemoCollectionPagerAdapter(
+//                        getSupportFragmentManager());
+//        mViewPager = (ViewPager) findViewById(R.id.pager);
+//        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+
+        var fireBase = FireBaseRealTime("service")
+        var textPercent = findViewById(R.id.averageInterest) as TextView
+        starPercent5 = findViewById(R.id.starPercent5) as TextView
+        starPercent4 = findViewById(R.id.starPercent4) as TextView
+        starPercent3 = findViewById(R.id.starPercent3) as TextView
+        starPercent2 = findViewById(R.id.starPercent2) as TextView
+        starPercent1 = findViewById(R.id.starPercent) as TextView
+        allReview = findViewById(R.id.allReview) as TextView
+        tabLikeNumber = findViewById(R.id.likesNumber) as TextView
+        tabDislikeNumber = findViewById(R.id.dislikesNumber) as TextView
+
+        fireBase.allAverageLike(textPercent,
+                starPercent1,
+                starPercent2,
+                starPercent3,
+                starPercent4,
+                starPercent5,
+                allReview)
+
+        fireBase.allTabLikesAndDislike(tabLikeNumber,
+                tabDislikeNumber)
+
+//        fireBase.allStatictic()
+    }
 }
